4d142b35bea8b1fc0fb49672ae8163bbc99415ce
Michelle Luu <blueberryimp@MichellsMacBook.web-pass.com>
2017-10-21 17:55:07 -0700

    added Impishly class, greeting, props, and states WIP

--- src/App.js
+++ src/App.js
@@ -4,11 +4,14 @@ import React from 'react';
 import './App.css';
 import Impishly from './Impishly';
 
+
 //const app (takes no argument)
+//returns Impishly component 
 const App = () => {
   return (
     <div className="App">
-      <Impishly/>
+      <Impishly book="Neuromancer"/>
+      <Impishly book="Invisible Cities"/>
     </div>
   );
 };
--- src/Impishly.js
+++ src/Impishly.js
@@ -1,11 +1,30 @@
 //Impishly.js
 
-import React from 'react';
+//Component class from react modules
+import React, { Component } from 'react';
 import './Impishly.css';
 
-//const app (takes no argument)
-const Impishly = () => {
-  return (<div className="Impishly">Impishly!</div>);
-};
+class Impishly extends Component {
+    //instantiation, calls out to parent super 
+    constructor(props) {
+        super(props);
+        this.state = { greeting: 'I read ' };
+        this.notRead = this.notRead.bind(this);
+    }
 
+notRead() {
+  this.setState({ greeting: "I did not read " });
+}
+
+  render() {
+    return (
+      <div className="Impishly">{this.state.greeting}
+        {this.props.book}!
+            <br/>
+            <button onClick=
+        {this.notRead}>Not Read!</button>
+            </div>
+    );
+  }
+}
 export default Impishly;
