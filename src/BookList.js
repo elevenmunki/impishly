04ddd4da9e58929279178c1b5e65d0c1e26a47f3
Michelle Luu <blueberryimp@Michelles-MacBook.local>
2017-10-23 17:37:30 -0700

    added key to booklist WIP

--- src/BookList.js
+++ src/BookList.js
@@ -7,8 +7,13 @@ class BookList extends Component {
     constructor(props) { 
         super(props);
         //gets greeting from impishly
-        this.state = { greetings: ['Neuromancer', 'Invisible Cities', "Apples", "New Rose Hotel"] };
+        this.state = { greetings: ['Neuromancer', 'Invisible Cities', "Half Magic", "Calvin and Hobbes", "XKCD", "Jane Eyre", "New Rose Hotel"] };
     }
+
+//for React to know which element to modify/remove/etc when one of 
+//the elements in your list changes, it has to be able to uniquely 
+//identify which element it is, so here we’re specifying the key as the book.
+
 renderGreetings() {
   return this.state.greetings.map(book => (
     <Impishly key={book} book={book}/>
